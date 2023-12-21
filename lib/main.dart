dd0234b2f6c88349426b51a4891f7c119fb2b430
sakthivel <sakthivel.bpigt@gmail.com>
2023-12-21 19:09:12 +0530

    content dialog for specification window works fine

--- lib/main.dart
+++ lib/main.dart
@@ -42,20 +42,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String? result = '';
 
-  var numberType = NumberTypes.Natural;
-  var number = Numbers.ONE;
-
   @override
   Widget build(BuildContext context) {
     return ScaffoldPage.scrollable(
       header: const PageHeader(title: Text('ContentDialog')),
       children: [
-        Button(
-          child: const Text('Show dialog'),
-          onPressed: () => showContentDialog(context),
+        Center(
+          child: Button(
+            child: const Text('Show dialog'),
+            onPressed: () => showContentDialog(context),
+          ),
         ),
         const SizedBox(width: 10.0),
-        Text(result ?? ''),
       ],
     );
   }
@@ -65,39 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
       context: context,
       builder: (context) => ContentDialog(
         title: const Text('Content Dialog'),
-        content: Column(
-          children: [
-            ComboBox<String>(
-              value: numberType,
-              items: NumberTypes.NumberTypesList.map<ComboBoxItem<String>>((e) {
-                return ComboBoxItem<String>(value: e, child: Text(e));
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  numberType = newValue!;
-                  debugPrint(numberType);
-                });
-              },
-            ),
-            Text('Selected item is $numberType'),
-            if (numberType.contains(NumberTypes.Natural))
-              ComboBox<String>(
-                value: number,
-                items: Numbers.NumberList.map<ComboBoxItem<String>>((e) {
-                  return ComboBoxItem<String>(
-                    value: e,
-                    child: Text(e),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    number = newValue!;
-                    debugPrint(number);
-                  });
-                },
-              ),
-          ],
-        ),
+        content: const NumberDialog(),
         actions: [
           Button(
             child: const Text('Ok'),
@@ -117,6 +83,136 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class NumberDialog extends StatefulWidget {
+  const NumberDialog({super.key});
+
+  @override
+  State<NumberDialog> createState() => _NumberDialogState();
+}
+
+class _NumberDialogState extends State<NumberDialog> {
+  var numberType = NumberTypes.Natural;
+  var number = Numbers.ONE;
+  Map<String, bool> l1 = {'a': true, 'b': true, 'c': true};
+  Map<String, bool> l2 = {'d': true, 'e': true, 'f': true};
+  Map<String, bool> l3 = {'g': true, 'h': true, 'i': true};
+  Map<String, bool> l4 = {'j': true, 'k': true, 'l': true};
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          ComboBox<String>(
+            value: numberType,
+            items: Security.securityList.map<ComboBoxItem<String>>((e) {
+              return ComboBoxItem<String>(value: e, child: Text(e));
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                numberType = newValue!;
+                debugPrint(numberType);
+              });
+            },
+          ),
+          if (numberType == Security.Cryptos)
+            Container(
+              width: 200,
+              height: 400,
+              padding: const EdgeInsets.all(10.0),
+              child: ListView.builder(
+                itemCount: l1.length,
+                itemBuilder: (context, index) {
+                  String key = l1.keys.elementAt(index);
+                  return ListTile(
+                    title: Text(key),
+                    trailing: Checkbox(
+                      onChanged: (bool? value) {
+                        setState(() {
+                          l1[key] = value!;
+                        });
+                      },
+                      checked: l1[key],
+                    ),
+                  );
+                },
+              ),
+            ),
+          if (numberType == Security.FX_pairs)
+            Container(
+              width: 200,
+              height: 400,
+              padding: const EdgeInsets.all(10.0),
+              child: ListView.builder(
+                itemCount: l2.length,
+                itemBuilder: (context, index) {
+                  String key = l2.keys.elementAt(index);
+                  return ListTile(
+                    title: Text(key),
+                    trailing: Checkbox(
+                      onChanged: (bool? value) {
+                        setState(() {
+                          l2[key] = value!;
+                        });
+                      },
+                      checked: l2[key],
+                    ),
+                  );
+                },
+              ),
+            ),
+          if (numberType == Security.Indices_Comm)
+            Container(
+              width: 200,
+              height: 400,
+              padding: const EdgeInsets.all(10.0),
+              child: ListView.builder(
+                itemCount: l3.length,
+                itemBuilder: (context, index) {
+                  String key = l3.keys.elementAt(index);
+                  return ListTile(
+                    title: Text(key),
+                    trailing: Checkbox(
+                      onChanged: (bool? value) {
+                        setState(() {
+                          l3[key] = value!;
+                        });
+                      },
+                      checked: l3[key],
+                    ),
+                  );
+                },
+              ),
+            ),
+          if (numberType == Security.Metals_and_Oil_pairs)
+            Container(
+              width: 200,
+              height: 400,
+              padding: const EdgeInsets.all(10.0),
+              child: ListView.builder(
+                itemCount: l4.length,
+                itemBuilder: (context, index) {
+                  String key = l4.keys.elementAt(index);
+                  return ListTile(
+                    title: Text(key),
+                    trailing: Checkbox(
+                      onChanged: (bool? value) {
+                        setState(() {
+                          l4[key] = value!;
+                        });
+                      },
+                      checked: l4[key],
+                    ),
+                  );
+                },
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
+
 class Numbers {
   static const String ONE = "One";
   static const String TWO = "Two";
@@ -132,4 +228,18 @@ class NumberTypes {
   static const String Irrational = "Irrational";
 
   static List<String> NumberTypesList = [Natural, Real, Rational, Irrational];
+}
+
+class Security {
+  static const String Cryptos = "Cryptos";
+  static const String FX_pairs = "FX pairs";
+  static const String Indices_Comm = "Indices & Comm";
+  static const String Metals_and_Oil_pairs = "Metals and Oil pairs";
+
+  static List<String> securityList = [
+    Cryptos,
+    FX_pairs,
+    Indices_Comm,
+    Metals_and_Oil_pairs
+  ];
 }
